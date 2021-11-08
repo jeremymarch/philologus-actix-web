8c6df1e1a52beaf099ad537d21e08b65a010dc23
jeremymarch <jmarch@gradcenter.cuny.edu>
2021-11-07 22:13:49 -0500

    update ip for render

--- src/main.rs
+++ src/main.rs
@@ -239,7 +239,7 @@ async fn main() -> io::Result<()> {
             //.service(fs::Files::new("/{lex}/{word:[^.{}/]+}", "static").prefer_utf8(true).index_file("index.html"))
             
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8088")?
     .run()
     .await
 }

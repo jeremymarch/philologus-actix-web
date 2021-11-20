8ebdfd80d23479ebb7dd67f02073ba951c2fe93e
jeremymarch <jmarch@gradcenter.cuny.edu>
2021-11-19 19:47:41 -0500

    add more error checks for sqlx queries

--- src/db.rs
+++ src/db.rs
@@ -17,7 +17,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>. 
 */
 
-use actix_web::{Error as AWError};
+//use actix_web::{Error as AWError};
+use actix_web::{HttpResponse, ResponseError, http::StatusCode};
+use thiserror::Error;
 
 use sqlx::sqlite::SqliteRow;
 use sqlx::{FromRow, Row, SqlitePool };
@@ -46,66 +48,36 @@ pub struct DefRow {
     pub seq: u32
 }
 
-pub async fn get_def_by_word(pool: &SqlitePool, table:&str, word:&str) -> Result<Option<DefRow>, AWError> {
+pub async fn get_def_by_word(pool: &SqlitePool, table:&str, word:&str) -> Result<Option<DefRow>, PhilologusError> {
     let decoded_word = percent_decode_str(word).decode_utf8().unwrap();
     let query = format!("{}{}{}{}{}", "SELECT word,sortword,def,seq FROM ", table, " WHERE word = '", decoded_word, "' LIMIT 1;");
 
     let rec = sqlx::query_as::<_, DefRow>(&query)
     .fetch_one(&*pool)
-    .await;
-    /*
-    match rec {
-        Ok(rec) => {
-        let res = match rec {
-            None => return Ok(None),
-            Some(rec) => {
-                return Ok(Some(DefRow {
-                    word: rec.unwrap().get(0),
-                    sortword: rec.unwrap().get(1),
-                    def: rec.unwrap().get(2),
-                    seq: rec.unwrap().get(3)
-                }))
-            }
-        }}
-        Err() => return Err()
-    }*/
-    Ok(Some(rec.unwrap()))
+    .await.map_err(map_sqlx_error)?;
+
+    Ok(Some(rec))
 }
 
-pub async fn get_def_by_seq(pool: &SqlitePool, table:&str, id:u32) -> Result<Option<DefRow>, AWError> {
+pub async fn get_def_by_seq(pool: &SqlitePool, table:&str, id:u32) -> Result<Option<DefRow>, PhilologusError> {
     let query = format!("{}{}{}{}{}", "SELECT word,sortword,def,seq FROM ", table, " WHERE seq = ", id, " LIMIT 1;");
 
     let rec = sqlx::query_as::<_, DefRow>(&query)
     .fetch_one(&*pool)
-    .await;
+    .await.map_err(map_sqlx_error)?;
 
-    /*match rec {
-        Ok(rec) => {
-        let res = match rec {
-            None => return Ok(None),
-            Some(rec) => {
-                return Ok(Some(DefRow {
-                    word: rec.unwrap().get(0),
-                    sortword: rec.unwrap().get(1),
-                    def: rec.unwrap().get(2),
-                    seq: rec.unwrap().get(3)
-                }))
-            }
-        },
-        Err() => return Err()
-    }*/
-    Ok(Some(rec.unwrap()))
+    Ok(Some(rec))
 }
 
 //, SEQ_COL, $table, UNACCENTED_COL, $word, STATUS_COL, UNACCENTED_COL);
-pub async fn get_seq_by_prefix(pool: &SqlitePool, table:&str, word:&str) -> Result<u32, AWError> {
+pub async fn get_seq_by_prefix(pool: &SqlitePool, table:&str, word:&str) -> Result<u32, PhilologusError> {
     let query = format!("{}{}{}{}{}", "SELECT seq,word,def,sortword FROM ", table, " WHERE sortword >= '", word, "' ORDER BY sortword LIMIT 1;");
     
     let rec = sqlx::query_as::<_, DefRow>(&query)
     .fetch_one(&*pool)
-    .await;
+    .await.map_err(map_sqlx_error)?;
 
-    Ok(rec.unwrap().seq)
+    Ok(rec.seq)
     /*
     match rec {
         Ok(rec) => return rec,
@@ -126,38 +98,33 @@ pub async fn get_seq_by_prefix(pool: &SqlitePool, table:&str, word:&str) -> Resu
 }
 
 //, SEQ_COL, $table, UNACCENTED_COL, $word, STATUS_COL, UNACCENTED_COL);
-pub async fn get_seq_by_word(pool: &SqlitePool, table:&str, word:&str) -> Result<u32, AWError> {
+pub async fn get_seq_by_word(pool: &SqlitePool, table:&str, word:&str) -> Result<u32, PhilologusError> {
     let decoded_word = percent_decode_str(word).decode_utf8().unwrap();
     let query = format!("{}{}{}{}{}", "SELECT seq,word,def,sortword FROM ", table, " WHERE word = '", decoded_word, "' LIMIT 1;");
 
     let rec = sqlx::query_as::<_, DefRow>(&query)
     .fetch_one(&*pool)
-    .await;
+    .await.map_err(map_sqlx_error)?;
 
-    Ok(rec.unwrap().seq)
+    Ok(rec.seq)
 }
 
 //, ID_COL, WORD_COL, $table, $tagJoin, SEQ_COL, $middleSeq, STATUS_COL, $tagwhere, SEQ_COL, $req->limit * $req->page * -1, $req->limit);
-pub async fn get_before(pool: &SqlitePool, table:&str, seq: u32, page: i32, limit: u32) -> Result<Vec<QueryResults>, AWError> {
+pub async fn get_before(pool: &SqlitePool, table:&str, seq: u32, page: i32, limit: u32) -> Result<Vec<QueryResults>, PhilologusError> {
     let query = format!("{}{}{}{}{}{}{}{}{}", "SELECT seq,word FROM ", table, " WHERE seq < ", seq, " ORDER BY seq DESC LIMIT ", page * limit as i32 * -1, ",", limit, ";");
-        let res: Result<Vec<QueryResults>, sqlx::Error> = sqlx::query(&query)
-        .map(|rec: SqliteRow| QueryResults {
-            i: rec.get("seq"),
-            r: (rec.get("word"), rec.get("seq"), 0)
-        })
-        .fetch_all(pool)
-        .await;
+    let res: Result<Vec<QueryResults>, sqlx::Error> = sqlx::query(&query)
+    .map(|rec: SqliteRow| QueryResults {
+        i: rec.get("seq"),
+        r: (rec.get("word"), rec.get("seq"), 0)
+    })
+    .fetch_all(pool)
+    .await;
 
-        Ok(res.unwrap()) //fake for now
-        /*
-        match res {
-            Ok(res) => return Ok(res),
-            _ => return Err(res).map_err(AWError::from)
-        };*/
+    res.map_err(map_sqlx_error)
 }
 
 //, ID_COL, WORD_COL, $table, $tagJoin, SEQ_COL, $middleSeq, STATUS_COL, $tagwhere, SEQ_COL, $req->limit * $req->page, $req->limit);
-pub async fn get_equal_and_after(pool: &SqlitePool, table:&str, seq: u32, page: i32, limit: u32) -> Result<Vec<QueryResults>, AWError> {
+pub async fn get_equal_and_after(pool: &SqlitePool, table:&str, seq: u32, page: i32, limit: u32) -> Result<Vec<QueryResults>, PhilologusError> {
     let query = format!("{}{}{}{}{}{}{}{}{}", "SELECT seq,word FROM ", table, " WHERE seq >= ", seq, " ORDER BY seq LIMIT ", page * limit as i32, ",", limit, ";");
     let res: Result<Vec<QueryResults>, sqlx::Error> = sqlx::query(&query)
     .map(|rec: SqliteRow| QueryResults {
@@ -167,18 +134,70 @@ pub async fn get_equal_and_after(pool: &SqlitePool, table:&str, seq: u32, page: 
     .fetch_all(pool)
     .await;
 
-    Ok(res.unwrap()) //fake for now
-    /*
-    match res {
-        Ok(res) => return Ok(res),
-        _ => return Err(res).map_err(AWError::from)
-    };*/
+    res.map_err(map_sqlx_error)
 }
 
-/*
-impl ResponseError for Error {
-    fn error_response(&self) -> HttpResponse {
-        HttpResponse::build(StatusCode::INTERNAL_SERVER_ERROR).finish()
+#[derive(Error, Debug)]
+pub enum PhilologusError {
+    #[error("Requested file was not found")]
+    NotFound,
+    #[error("You are forbidden to access requested file.")]
+    Forbidden,
+    #[error("Unknown Internal Error")]
+    Unknown
+}
+impl PhilologusError {
+    pub fn name(&self) -> String {
+        match self {
+            Self::NotFound => "NotFound".to_string(),
+            Self::Forbidden => "Forbidden".to_string(),
+            Self::Unknown => "Unknown".to_string(),
+        }
     }
 }
-*/
+impl ResponseError for PhilologusError {
+    fn status_code(&self) -> StatusCode {
+        match *self {
+            Self::NotFound  => StatusCode::NOT_FOUND,
+            Self::Forbidden => StatusCode::FORBIDDEN,
+            Self::Unknown => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+
+    fn error_response(&self) -> HttpResponse {
+        let status_code = self.status_code();
+        let error_response = ErrorResponse {
+            code: status_code.as_u16(),
+            message: self.to_string(),
+            error: self.name(),
+        };
+        HttpResponse::build(status_code).json(error_response)
+    }
+}
+
+fn map_sqlx_error(_e: sqlx::Error) -> PhilologusError {   
+    /*
+    match e { //.kind() {
+        sqlx::Error::Io(Error) => PhilologusError::Unknown,
+        sqlx::Error::UrlParse(_) => PhilologusError::Unknown,
+        sqlx::Error::Database(err) => PhilologusError::Unknown,
+        sqlx::Error::NotFound => PhilologusError::Unknown,
+        sqlx::Error::FoundMoreThanOne => PhilologusError::Unknown,
+        sqlx::Error::ColumnNotFound(err) => PhilologusError::Unknown,
+        sqlx::Error::Protocol(err) => PhilologusError::Unknown,
+        sqlx::Error::PoolTimedOut => PhilologusError::Unknown,
+        sqlx::Error::PoolClosed => PhilologusError::Unknown,
+        sqlx::Error::Decode(err) => PhilologusError::Unknown,
+        // some variants omitted
+        _ => PhilologusError::Unknown,
+    }
+    */
+    PhilologusError::Unknown
+}
+
+#[derive(Serialize)]
+struct ErrorResponse {
+    code: u16,
+    error: String,
+    message: String,
+}
